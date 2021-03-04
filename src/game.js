3913398e1a46378975f56f8f0e9682a80c7b5698
Sarah Fitzsimons <sarahfitz2713@gmail.com>
2021-03-04 16:21:23 -0700

    Create classes with methods for game and player

--- src/game.js
+++ src/game.js
@@ -1,1 +1,22 @@
-// 
+class Game {
+    constructor() {
+
+        this.playersTurn = playersTurn;
+    }
+    evaluateBoard() {
+        //win/lose/draw ?
+    }
+
+    resetBoard() {
+        //timer
+    }
+}
+
+// A Game should include:
+// Two Player instances
+// A way to keep track of the data for the game board
+// A way to keep track of which player’s turn it currently is
+// A way to check the Game’s board data for win conditions
+// A way to detect when a game is a draw(no one has won)
+// A way to save a winning Game’s board data to the correct player’s wins array
+// A way to reset the Game’s board to begin a new game
