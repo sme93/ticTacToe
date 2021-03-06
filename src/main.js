c64a435d4aa8fa07eb4534120a1c7302251de22e
Sarah Fitzsimons <sarahfitz2713@gmail.com>
2021-03-06 10:51:38 -0700

    Add renderCurrentPlayersTurn and renderWins functions

--- src/main.js
+++ src/main.js
@@ -2,26 +2,45 @@
 //handle move
 
 var gameBoard = document.querySelector("#gameBoard");
-console.log("gameboard", gameBoard);
+var currentGameState = document.querySelector("#currentGameState");
+
 
 gameBoard.addEventListener("click", handleTurn);
+window.addEventListener("load", startGame);
+
+function startGame() {
+    renderWins();
+    renderCurrentPlayersTurn();
+}
+
+function renderWins() {
+   var playerOneWins = playerOne.retrieveWinsFromStorage();
+   var playerTwoWins = playerTwo.retrieveWinsFromStorage();
+   //get from storage, print to page
+}
+
+function renderCurrentPlayersTurn() {
+    currentGameState.innerHTML = `${newGame.playersTurn}'s turn`
+}
 
 function handleTurn(event) {
     var target = parseInt(event.target.id);
-    console.log(target);
+ console.log("target ", target);
     newGame.toggleTurn();
+ console.log("newGame ", newGame);
+ renderCurrentPlayersTurn();
 }
 
 var playerOne = new Player("one", "emoji", []);
 var playerTwo = new Player("two", "emoji2", []);
 
-console.log(playerOne);
-playerOne.saveWinsToStorage(['x', 'x', 'x', 'o', 'o', 'x', 'o', 'x', 'o']);
-playerTwo.saveWinsToStorage(['x', 'x', 'x', 'o', 'o', 'x', 'o', 'x', 'x']);
-playerOne.saveWinsToStorage([null, null, null, "o", "o", "o", null, "x", "x"]);
-playerTwo.saveWinsToStorage(["o", "o", "o", null, "x", null, "x", null, null]);
+// console.log(playerOne);
+// playerOne.saveWinsToStorage(['x', 'x', 'x', 'o', 'o', 'x', 'o', 'x', 'o']);
+// playerTwo.saveWinsToStorage(['x', 'x', 'x', 'o', 'o', 'x', 'o', 'x', 'x']);
+// playerOne.saveWinsToStorage([null, null, null, "o", "o", "o", null, "x", "x"]);
+// playerTwo.saveWinsToStorage(["o", "o", "o", null, "x", null, "x", null, null]);
 
-console.log(playerOne.retrieveWinsFromStorage()); 
+// console.log(playerOne.retrieveWinsFromStorage()); 
 
 var newGame = new Game("x");
 newGame.evaluateBoard();
